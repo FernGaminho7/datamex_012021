258ed229ffa0d7ee638fa06d9c9d997b777d0906
Fernando Gamino <fergamino96@gmail.com>
2021-01-18 23:11:15 -0600

    mini proyecto semana 1

---
+++ module-1/lab-mysql/your-code/lab_mysql.sql
@@ -0,0 +1,38 @@
+/*create database cars_database;
+ use cars_database;
+
+CREATE table cars_database.cars(
+VIN VARCHAR(25),
+Manufacturer VARCHAR(15),
+Model VARCHAR(30),
+Año varchar(4), #Le cambié de int a varchar
+Color VARCHAR(10)
+); 
+create table cars_database.customers(
+Customer_id varchar(5),
+Nombre varchar(40),
+Phone varchar(15),
+Email varchar(20),
+Adress varchar(30),
+City varchar(20),
+State varchar(20),
+Country varchar(20),
+ZIP_code varchar(5)
+);
+
+create table cars_database.salespersons(
+Staff_id varchar(5),
+Nombre varchar(40),
+Store varchar(20)
+);
+
+create table cars_database.invoice(
+Invoice_number varchar(9),
+Selling_date date,
+Car int,
+Customer int,
+Salesperson int
+); */
+
+## Challenge 3
+
